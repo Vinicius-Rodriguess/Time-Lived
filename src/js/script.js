9b4a29ae44afb90397176d9aab265578b2a83220
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-07-20 16:32:05 -0300

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,25 @@
 function enviar() {
-  const dia = document.querySelector(".dia").value;
-  const mes = document.querySelector(".mes").value;
-  const ano = document.querySelector(".ano").value;
-  const form = document.querySelector(".form");
+  const dia = parseInt(document.querySelector(".dia").value);
+  const mes = parseInt(document.querySelector(".mes").value);
+  const ano = parseInt(document.querySelector(".ano").value);
   const resultado = document.querySelector(".p-resultado");
   const divResultado = document.querySelector(".divResultado");
   const nascimento = new Date(ano, mes - 1, dia);
   const atual = new Date();
 
-  if (dia < 1 || dia > 31) {
-    alert("Insira um dia valido!");
-    return;
-  } else if (mes < 1 || mes > 12) {
-    alert("insira um mes valido!");
-    return;
-  } else if (ano > atual.getFullYear) {
-    alert("insira um ano valido!");
+  function validarData(dia, mes, ano) {
+    const data = new Date(ano, mes - 1, dia);
+    return data.getDate() === dia && data.getMonth() === mes - 1 && data.getFullYear() === ano;
+  }
+
+  if (!validarData(dia, mes, ano) || ano < 0 || ano > atual.getFullYear()) {
+    alert("Insira uma data válida!");
     return;
   }
 
-  const diffMilissegundos = atual - nascimento;
+  const difMilissegundos = atual - nascimento;
 
-  const diffDias = diffMilissegundos / (24 * 60 * 60 * 1000);
+  const diffDias = difMilissegundos / (24 * 60 * 60 * 1000);
 
   const anos = Math.floor(diffDias / 365);
   const meses = Math.floor((diffDias % 365) / 30);
@@ -31,10 +29,6 @@ function enviar() {
   resultado.innerHTML = `<p> Você tem ${anos} anos, ${meses} meses e ${dias} dias de vida. </p> <br>`;
   resultado.innerHTML += `<p> ${Math.floor(diffDias)} Dias </p> <br>`;
   resultado.innerHTML += `<p> ${Math.floor(diffDias * 24)} Horas </p> <br>`;
-  resultado.innerHTML += `<p> ${Math.floor(
-    diffDias * 24 * 60
-  )} Minutos </p> <br>`;
-  resultado.innerHTML += `<p> ${Math.floor(
-    diffDias * 24 * 60 * 60
-  )} Segundos </p> <br>`;
+  resultado.innerHTML += `<p> ${Math.floor(diffDias * 24 * 60)} Minutos </p> <br>`;
+  resultado.innerHTML += `<p> ${Math.floor(diffDias * 24 * 60 * 60)} Segundos </p> <br>`;
 }
