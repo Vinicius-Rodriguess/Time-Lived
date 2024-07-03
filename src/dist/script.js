4145b445430d3d99782829a8fc54491b04ec9298
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2024-07-03 20:35:59 -0300

    update css nesting

--- src/dist/script.js
+++ src/dist/script.js
@@ -1,12 +1,17 @@
 "use strict";
 const btnCalc = document.querySelector("#btn-calc");
 const inputDate = document.querySelector("#input-date");
-btnCalc.addEventListener("click", () => createResults(inputDate));
+inputDate.addEventListener("input", () => inputDate.classList.remove("error"));
+btnCalc.addEventListener("click", () => { createResults(inputDate); });
 const createResults = (inputElement) => {
     if (inputElement.value === "")
         return;
     const currentDate = new Date();
     const birthDate = new Date(inputElement.value);
+    if (birthDate > currentDate) {
+        inputDate.classList.add("error");
+        return;
+    }
     const results = {
         birthday: formatDate(birthDate),
         seconds: `${calcSeconds(currentDate, birthDate)} segundos`,
@@ -35,16 +40,25 @@ const calcYears = (date1, date2) => date1.getFullYear() - date2.getFullYear();
 const calcTimeLived = (date1, date2) => {
     const years = calcYears(date1, date2);
     const months = date1.getMonth() - date2.getMonth();
-    const days = date1.getDate() - date2.getDate();
+    const days = date1.getDate() > date2.getDate() ? date1.getDate() - date2.getDate() : date2.getDate() - date1.getDate();
     return `Você tem ${years} anos, ${months} meses e ${days} dias de vida!`;
 };
 const renderResults = (results) => {
     const containerResults = document.querySelector(".container-results");
     containerResults.innerHTML = "";
-    for (let i in results) {
+    let i;
+    for (i in results) {
         const text = document.createElement("p");
         text.innerText = results[i];
         containerResults.appendChild(text);
     }
     containerResults.classList.remove("hide");
 };
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add(`animate-${entry.target.dataset.animate}`, 'played');
+        }
+    });
+}, { rootMargin: '-80px' });
+document.querySelectorAll('[data-animate]').forEach(el => observer.observe(el));
